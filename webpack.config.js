d1597988d2011f9aaf318475199990061f084141
Marcos Juan Juan <webbegg@gmail.com>
2021-01-06 11:49:08 +0100

    webpack + dependencies added

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/game.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8000,
+  },
+}
